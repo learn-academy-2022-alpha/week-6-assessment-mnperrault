604f0ea2e196f1f62fcec18d9d1d76227dd2579c
mnperrault <perrault.meaghan@gmail.com>
2022-04-04 08:13:30 -0700

    Completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -19,10 +19,48 @@ const people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("sentenceBuilder", () => {
+
+  // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+  it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized.", () => {
+    const people = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" }
+    ]
+
+    //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+    expect(sentenceBuilder(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+// sentenceBuilder › takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+//  ReferenceError: sentenceBuilder is not defined
+// Good failure
 
 
 // b) Create the function that makes the test pass.
+
+// Declare a function that takes in array.
+// Declare a variable that uses the .map function and the .toUpperCase that uses the values of name and occupation in a string.
+// Return the string.
+
+const senteS = (array) => {
+  let sentence = array.map(person => {
+    let splitName = person.name.split(" ")
+    let firstName = (splitName[0][0].toUpperCase()+splitName.slice(0,1)).split("")
+    firstName.splice(1,1)
+    let lastName = (splitName[1][0].toUpperCase()+splitName.slice(1,2)).split("")
+    lastName.splice(1,1)
+    return firstName.join("")+" "+lastName.join("")+" is "+ person.occupation+"."
+  })
+  return sentence
+}
+
+// The test passed Green.
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
@@ -33,9 +71,43 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("only3Remainders", () => {
+
+  // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    // Expected output: [ 2, 0, -1, 0 ]
+    const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    // Expected output: [ 2, 1, -1 ]
+
+    //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+    expect(only3Remainders(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(only3Remainders(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
+// only3Remainders › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+// ReferenceError: only3Remainders is not defined
+// Good failure.
+
 
 
 // b) Create the function that makes the test pass.
+
+// Create a function that takes in a array.
+// Use the filter method to complie the idex of the values that are integers.
+// Use the .map to iterate through the integers and and modoel to divide by 3.
+// Return the value.
+
+const only3Remainders = (array) => {
+  let remainders = array.filter(value => {
+    return typeof value === "number"
+  })
+  return remainders.map(value => value%3)
+}
+
+// The test passed Green.
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
@@ -46,6 +118,38 @@ const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("cubedSum", () => {
 
+  // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+  it("takes in an array of numbers and returns the sum of all the numbers cubed.", () => {
+    const cubeAndSum1 = [2, 3, 4]
+    // Expected output: 99
+    const cubeAndSum2 = [0, 5, 10]
+    // Expected output: 1125
+
+    //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+    expect(cubedSum(cubeAndSum1)).toEqual(99)
+    expect(cubedSum(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+// cubedSum › takes in an array of numbers and returns the sum of all the numbers cubed.
+// ReferenceError: cubedSum is not defined
+// Good failure
 
 // b) Create the function that makes the test pass.
+
+// Declare a function that takes in a array 
+// Declare a const that uses the .map method and iterates through the array taking it to the 3rd power
+// Use that variable and the .reduce method with the direction to add the values to get one sum 
+// Return answer rendering .reduce method.
+
+const cubedSum = (array) => {
+  let answer = array.map(value => {
+    return value ** 3
+  })
+  return answer.reduce((a,b) => a+b)
+}
+
+// The test passed Green.
--- interview-questions.md
+++ interview-questions.md
@@ -1,4 +1,5 @@
 # ASSESSMENT 6: Interview Practice Questions
+
 Answer the following questions.
 
 First, without external resources. Challenge yourself to answer from memory.
@@ -8,43 +9,74 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
   Your answer:
+  Your answer: Cohort and Student are two different data tables that are being linked to each other. This mistake can be fixed by incorporating the belongs_to statement in the Student model. If the Cohort model contains the has_many statement, then it makes sense for the student model to have this statement.
 
   Researched answer:
-
-
+  Researched answer: The foreign key must be assigned to a model that is plural, not singular. In this example, the plural model is Student because a cohort has many students. The name of the foreign key would be student_id because the naming convention for foreign keys is modelname_id.
 
 2. Which RESTful routes must always be passed params? Why?
 
   Your answer:
+  Your answer: The main routes that must always be passed are the show, create, update, and delete routes. These routes involve executing a certain action on a specific piece of data. When learning about RESTful routes, I remember always having to use the find() method while passing the params.
 
   Researched answer:
-
-
+  Researched answer: After researching I realized  that the routes that must always be included are the .show, .edit, and .destroy parameters.
 
 3. Name three rails generator commands. What is created by each?
 
   Your answer:
+  Your answer: The rails generator commands are responsible for creating the Model and Controller for a rails application. The routes.rb file does not need to be updated if the generate Resource command was ran. It is similar to the rails generate Model command but adds more dependencies to the app.
 
   Researched answer:
-
-
+  Researched answer: The rails generate model command is responsible for creating the name of a model. The rails generate Controller command adds more important files to a rails app. These files can then be modified so that the controller can route external requests to internal actions. All of the files get added in one command making it a lot more utility.
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
-action: "GET"    location: /students          
+action: "GET"    location: /students
+The INDEX method would display all of the rows in the database.
 
-action: "POST"   location: /students       
+action: "POST"   location: /students
+Controller method: index - The index method would show all of the students currently in the database.
+
+action: "POST"   location: /students
+Controller method: create - The create method would input new data into the database.
 
 action: "GET"    location: /students/new
+Controller method: new - The new method would show a form to input a new student into the database.
 
 action: "GET"    location: /students/2  
+Controller method: show - The show method would shoe only one student depending on the params that was passed in the url.
 
-action: "GET"    location: /students/2/edit    
+action: "GET"    location: /students/2/edit
+Controller method: edit - The edit method is similar to the new method and would show a form to edit information about a student that is already in the database.
 
-action: "PATCH"  location: /students/2      
+action: "PATCH"  location: /students/2
+The UPDATE method would be passed changes as arguments. The row with the primary key "2" is what the arguments would update.
 
-action: "DELETE" location: /students/2      
+action: "DELETE" location: /students/2
+Controller method: update - The update method would modify an existing student in the database.
+action: "DELETE" location: /students/2
 
-
+Controller method: destroy - The destroy method would remove a student from the database.
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+1. As a user, I can see a list of all the tasks on the home page of the application.
+
+2. As a user, I can click on a task on the home page and be directed to a page where I can see the completion status of the task that I have clicked.
+
+3. As a user, I can navigate from the show page back to the home page.
+
+4. As a user, I can click a button that will take me from the home page to a page where I can add an additional task to the list.
+
+5. As a user, I see a form where I can create a new task to add to the list.
+
+6. As a user, I can update the title of the task on the to-do list.
+
+7. As a user, when I submit a new task, I am redirected to the home page.
+
+8. As a user, I can delete a tasks from the to-do list.
+
+9. As a user, I can update the completion status of tasks on the to-do list.
+
+10. As a user, I can click a button that will submit new tasks to the database.
